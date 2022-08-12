106c8e6bfe433b30efc3efbc9fd25f8599f4d7bb
pablo-lucas <pablo.lucv@gmail.com>
2022-08-12 10:40:06 -0500

    finishing up handler

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"stripeapp/internal/cards"
 )
 
 type stripePayload struct {
@@ -12,21 +14,61 @@ type stripePayload struct {
 
 type jsonResponse struct {
 	OK      bool   `json:"ok"`
-	Message string `json:"message"`
-	Content string `json:"content"`
-	ID      int    `json:"id"`
+	Message string `json:"message,omitempty"`
+	Content string `json:"content,omitempty"`
+	ID      int    `json:"id,omitempty"`
 }
 
 func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request) {
-	jResponse := jsonResponse{
-		OK: true,
-	}
+	var payload stripePayload
 
-	out, err := json.MarshalIndent(jResponse, "", "    ")
+	err := json.NewDecoder(r.Body).Decode(&payload)
+
 	if err != nil {
 		app.errorLog.Println(err)
+		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(out)
+	amount, err := strconv.Atoi(payload.Amount)
+
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	card := cards.Card{
+		Secret:   app.config.stripe.secret,
+		Key:      app.config.stripe.key,
+		Currency: payload.Currency,
+	}
+
+	okay := true
+	paymentIntent, msg, err := card.Charge(payload.Currency, amount)
+	if err != nil {
+		okay = false
+	}
+
+	if okay {
+		out, err := json.MarshalIndent(paymentIntent, "", "    ")
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	} else {
+		jResponse := jsonResponse{
+			OK:      false,
+			Message: msg,
+			Content: "",
+		}
+
+		out, err := json.MarshalIndent(jResponse, "", "    ")
+		if err != nil {
+			app.errorLog.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	}
 }
